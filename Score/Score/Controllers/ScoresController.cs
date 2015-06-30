319af65ba07ca01a3e71025c0a0ca488cfd956cc
zsx <zsx_njucser@163.com>
2015-07-01 05:28:40 +0800

    my app

---
+++ Score/Score/Controllers/ScoresController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Score.Models;
+
+namespace Score.Controllers
+{
+    public class ScoresController : Controller
+    {
+        private StudentScoreEntities1 db = new StudentScoreEntities1();
+
+        // GET: Scores
+        public ActionResult Index()
+        {
+            return View(db.Scores.ToList());
+        }
+
+        // GET: Scores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Scores scores = db.Scores.Find(id);
+            if (scores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scores);
+        }
+
+        // GET: Scores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Scores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,TheScore")] Scores scores)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    db.Scores.Add(scores);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(scores);                    
+                }
+
+            }
+            catch (Exception)
+            {
+                return View(scores);                
+            }
+
+        }
+
+        // GET: Scores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Scores scores = db.Scores.Find(id);
+            if (scores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scores);
+        }
+
+        // POST: Scores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,TheScore")] Scores scores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(scores).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(scores);
+        }
+
+        // GET: Scores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Scores scores = db.Scores.Find(id);
+            if (scores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scores);
+        }
+
+        // POST: Scores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Scores scores = db.Scores.Find(id);
+            db.Scores.Remove(scores);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
